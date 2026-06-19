e6b67ab26045272dde984624c096063aff9e7282
LucyAndLuna2023 <3023495@qq.com>
2026-06-19 17:59:53 +0800

    chore: remove core module self_modify.py from public repo

--- src/core/self_modify.py
+++
@@ -1,7 +0,0 @@
-"""Self Modify — 开源版 (stub)"""
-class _SelfModifyEngine:
-    def modify(self, *a, **kw): return None
-    def stats(self): return {}
-
-_engine = _SelfModifyEngine()
-def get_self_modify_engine(): return _engine
