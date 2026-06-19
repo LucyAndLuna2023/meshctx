166c96472ba0af3aae528db4687d8fc722951fc4
LucyAndLuna2023 <3023495@qq.com>
2026-06-19 17:59:44 +0800

    chore: remove core module brain_validator.py from public repo

--- src/core/brain_validator.py
+++
@@ -1,7 +0,0 @@
-"""Brain Validator — 开源版 (stub)"""
-class _BrainValidator:
-    def validate(self, *a, **kw): return True
-    def stats(self): return {}
-
-_validator = _BrainValidator()
-def get_brain_validator(): return _validator
